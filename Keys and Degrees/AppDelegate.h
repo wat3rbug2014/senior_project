df82b5d237e513c778b852cae71a55874fdc0245
Douglas Gardiner <doug.gardiner@gmail.com>
2014-08-29 17:36:32 -0500

    Initial Commit

---
+++ Keys and Degrees/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Keys and Degrees
+//
+//  Created by Douglas Gardiner on 8/29/14.
+//  Copyright (c) 2014 Douglas Gardiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
