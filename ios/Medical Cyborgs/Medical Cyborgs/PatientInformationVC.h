aa720e36defd1d2501c19fbfd085afc0902b55c0
Douglas Gardiner <doug.gardiner@gmail.com>
2014-10-21 09:29:23 -0500

    Renamed VC to something more appropriate.  Changed VC to use the device poller for gather information.

---
+++ ios/Medical Cyborgs/Medical Cyborgs/PatientInformationVC.h
@@ -0,0 +1,49 @@
+//
+//  SettingsVC.h
+//  Medical Cyborgs
+//
+//  Created by Douglas Gardiner on 10/9/14.
+//  Copyright (c) 2014 Douglas Gardiner. All rights reserved.
+//
+
+#import "VCWithSounds.h"
+#import "PersonalInfo.h"
+
+#define MONTH_COMP 0
+#define DAY_COMP 1
+#define YEAR_COMP 2
+#define NONE_FOUND 0
+
+
+
+@interface PatientInformationVC : VCWithSounds <UITextFieldDelegate,NSURLConnectionDelegate, NSURLConnectionDataDelegate>
+
+@property (retain, nonatomic) IBOutlet UITextField *firstNameEntry;
+@property (retain, nonatomic) IBOutlet UITextField *lastNameEntry;
+@property (retain, nonatomic) IBOutlet UIDatePicker *dobSelector;
+@property (retain, strong) PersonalInfo *patientData;
+@property (retain) NSMutableData *serverResponseData;
+
+
+/**
+ * This method creates a viewcontroller that has the personal information object.  The object queries
+ * the device store user default settings and maintains the records.  This information is needed
+ * for building the initial view with patient information.
+ *
+ * @param existingPatientData is the object that contains the patient name, DOB, and ID necessary
+ * for the VC to function.
+ * @return A viewcontroller that will display the current patient information and allow editing.
+ */
+
+-(id) initWithPersonalInformation:(PersonalInfo*) existingPatientData;
+
+/**
+ * This method updates the patient information  to reflect the date that is
+ * selected from the UIDatePicker object.
+ */
+
+-(void) updateDOB;
+
+
+
+@end
