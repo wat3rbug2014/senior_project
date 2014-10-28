54f76f9f63583983256b7c17e7fed592cb4d6773
Douglas Gardiner <doug.gardiner@gmail.com>
2014-10-28 10:17:30 -0500

    Added dummy data for database testing

---
+++ ios/Medical Cyborgs/Medical Cyborgs/DummyData.h
@@ -0,0 +1,19 @@
+//
+//  DummyData.h
+//  Medical Cyborgs
+//
+//  Created by Douglas Gardiner on 10/28/14.
+//  Copyright (c) 2014 Douglas Gardiner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyData : NSObject
+
+
+@property (readonly) NSInteger heartRate;
+@property (readonly) NSDate *timestamp;
+@property (readonly) float latitude;
+@property (readonly) float longitude;
+
+@end
