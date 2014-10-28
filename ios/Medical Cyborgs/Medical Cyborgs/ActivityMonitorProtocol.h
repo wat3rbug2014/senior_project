30003936a7196a5ae64aaf0c0398aa2970baf86d
Douglas Gardiner <doug.gardiner@gmail.com>
2014-10-28 11:30:03 -0500

    Added a protocol for the activity devices.  This will, no doubt be changed because some research is needed to see how the devices work.

---
+++ ios/Medical Cyborgs/Medical Cyborgs/ActivityMonitorProtocol.h
@@ -0,0 +1,40 @@
+//
+//  ActivityMonitorProtocol.h
+//  Medical Cyborgs
+//
+//  Created by Douglas Gardiner on 10/28/14.
+//  Copyright (c) 2014 Douglas Gardiner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ * This protocol is designed to be used on all activity monitor devices.
+ * The methods enclosed ensure uniform operation with the application, irrespective
+ * of the specific device.  It is used for polling so that all modifications are to the
+ * specific device and do not have cascading effects to the other devices.
+ */
+
+@protocol ActivityMonitorProtocol <NSObject>
+
+/**
+ * This method gets the current longitude value from the monitor that utilizes this
+ * protocol.
+ *
+ * @return A float of the current longitude from the activity monitor.
+ */
+
+-(float) getLongitude;
+
+
+/**
+ * This method gets the current latitude value from the monitor that utilizes this
+ * protocol.
+ *
+ * @return A float of the current latitude from the activity monitor.
+ */
+
+-(float) getLatitude;
+
+@end
