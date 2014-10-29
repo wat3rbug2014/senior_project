53f84708652b6b6b28fa6089d5a7bab449a5b16b
Douglas Gardiner <doug.gardiner@gmail.com>
2014-10-29 11:35:59 -0500

    Created a structure for passing local database results

---
+++ ios/Medical Cyborgs/Medical Cyborgs/LocalDBConstants.h
@@ -0,0 +1,22 @@
+//
+//  LocalDBConstants.h
+//  Medical Cyborgs
+//
+//  Created by Douglas Gardiner on 10/29/14.
+//  Copyright (c) 2014 Douglas Gardiner. All rights reserved.
+//
+
+#import <stdlib.h>
+#ifndef Medical_Cyborgs_LocalDBConstants_h
+#define Medical_Cyborgs_LocalDBConstants_h
+
+
+typedef struct {
+    
+    int heartRate;
+    float longitude;
+    float latitude;
+    double timestamp;
+    
+} DBResult;
+#endif
